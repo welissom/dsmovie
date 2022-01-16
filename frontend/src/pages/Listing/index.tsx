c14e44239c9e7d62cf371f7d17d21bffe2fdf363
Welisson Lima <welissomlima@hotmail.com>
2022-01-16 13:04:26 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,17 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Navbar/Pagination";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    return (
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);         
+        });
+
+    return (        
         <>
             <Pagination />
 
